666397bfb2df89674c601fe85c2f2242bfe15e25
Johnny Wong <johnnywong@leapoffrog.com>
2024-07-10 15:46:51 +0800

    1. Changed some wording 2. Modify the example 3. Added README and LICENCE

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -4,8 +4,8 @@ import "../globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Firstbot - Carpenter",
-  description: "Carpenter is an open-source AI project that writes and runs serverless code snippets in the cloud",
+  title: "Open Carpenter",
+  description: "Open Carpenter is an open-source AI project that writes and runs serverless code snippets in the cloud",
 };
 
 export default function RootLayout({ children }) {
